5849d1ff5772d6539a129a786dc2bf008dada48a
wada1980 <wada@osaka-pct.ac.jp>
2020-08-17 17:57:35 +0900

    クライアントからサーバーに送るコマンドデータをJSON形式に変更

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import logging
 import time
+import json
 
 logging.basicConfig(format='[%(levelname)s] %(asctime)s: %(message)s')
 log = logging.getLogger(__name__)
@@ -9,20 +10,41 @@ log.setLevel(level=logging.INFO)
 HOST = '127.0.0.1'
 PORT = 8889
 BUFFER_SIZE = 1024
+TIMEOUT = 5
 
+# dict型のオブジェクトを受け取ってJSONに変換して
+# DobotStudio側の PG にソケット通信により送る送信
 def send_command(command):
+  try :
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+      try :
+        s.connect((HOST, PORT))
+        s.settimeout(TIMEOUT)
+        s.send(json.dumps(command,ensure_ascii=False).encode())
+        log.info('"{0}"...'.format(command))
+        res = s.recv(BUFFER_SIZE).decode('UTF-8')
+      except socket.timeout:
+        log.error(f'タイムアウトしました。コマンド送信後、{TIMEOUT}経過してもレスポンスがありませんでした。')
+        return '[ERR] Timeout.'
+    log.info('   -> "{0}"'.format(res))
+  except ConnectionRefusedError:
+    log.error(f'{HOST}:{PORT} に接続できませんでした。Dobot側のプログラムが実行状態になっているか確認してください。') 
+    res = '[ERR] ConnectionRefused.'
+  return res
 
-  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.send(command.encode())
-    log.info('"{0}"...'.format(command))
-    res = s.recv(BUFFER_SIZE).decode('UTF-8')
-  log.info('   -> "{0}"'.format(res))
+j1 = dict(command='JUMP',x=400,y=0,z=50)
+j2 = dict(command='JUMP',x=350,y=0,z=50)
+j3 = dict(command='JAMP',x=350,y=0,z=50) # 誤ったコマンド
+j4 = dict(command='JUMP',x=350,y=0) # 誤ったコマンド
 
-send_command('JUMP TO 400 20 20')
+q = dict(command='QUIT')
+
+send_command(j1)
 time.sleep(2)
-send_command('JUMP TO 350 20 20')
+send_command(j2)
 time.sleep(2)
-send_command('JUMP TO 400 20 20')
+send_command(j3)
+send_command(j4)
+
 time.sleep(2)
-send_command('QUIT')
+send_command(q)
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import socket
 import logging
 import time
+import json
 
 logging.basicConfig(format='[%(levelname)s] %(asctime)s: %(message)s')
 log = logging.getLogger(__name__)
@@ -10,10 +11,30 @@ HOST = '127.0.0.1'
 PORT = 8889
 BUFFER_SIZE = 1024
 
-def exe_command(command):
-  time.sleep(0.5) # コマンド実行のダミー
-  res = 'OK'
-  log.info('"{0}"->"{1}"'.format(command,res))
+def exe_command(c):
+
+  if 'command' not in c :
+    msg= 'KeyError : Not found "command" key.'
+    log.error(msg)
+    return '[ERR] ' + msg 
+
+  try :
+    if c['command'] == 'JUMP' :
+      log.info('JUMP TO ({0},{1},{2})'.format(c['x'],c['y'],c['z']))
+      # dType.SetPTPCmdEx(api,c['x'],c['y'],c['z'],1)
+      res = '[OK]'
+    elif c['command'] == 'QUIT' :
+      log.info('QUIT')
+      res = '[OK]'
+    else :
+      msg = 'Unknown commad : {0}'.format(str(c['command']))
+      log.error(msg)
+      res = '[ERR] ' + msg
+  except KeyError:
+    msg = 'ArgError in "{0}". ({1})'.format(c['command'],c.keys())
+    log.error(msg)
+    res = '[ERR] ' + msg
+
   return res
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -23,12 +44,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     (connection, client) = s.accept()
     log.debug('Client connected {0}'.format(client))
     try:  
-      # コマンド受信
-      command = connection.recv(BUFFER_SIZE).decode('UTF-8')
-      # レスポンス
+      command = json.loads(connection.recv(BUFFER_SIZE).decode('UTF-8'))
       res = exe_command(command)
       connection.send(res.encode())
     finally:
       connection.close()
-      if command=='QUIT' :
+      if command['command']=='QUIT' :
         break
